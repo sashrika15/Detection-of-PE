a88a1548b992dab4619f476c9f4d257f761465eb
sashrika15 <52353601+sashrika15@users.noreply.github.com>
2020-02-25 22:24:48 +0530

    Update split.py

--- split.py
+++ split.py
@@ -1,9 +1,12 @@
+''' Split the dataset according to annotations given in json file'''
+
 import json
 import shutil 
 import os 
 name = []
 test = []
 count = 0
+# Path to data
 path = "/Users/sashrikasurya/Desktop/JPGs"
 for file in os.listdir(path):
     if file.endswith(".jpg"):
@@ -12,25 +15,22 @@ for file in os.listdir(path):
        #dst_dir = "your/dest/dir"
        #shutil.move(src_dir,dst_dir)
 train = []
-p = open("/Users/sashrikasurya/Desktop/P_Mask_RCNN-master/annotations/instances_val.json")
+# Path to json file
+p = open("/Users/sashrikasurya/Desktop/P_Mask_RCNN/annotations/instances_val.json")
 data = json.load(p)
 for i in data:
     for j in data['images']:
-        print(j['file_name'])
+        #print(j['file_name'])
         #if j['file_name'].endswith(".jpg"):
         #    count = count+1
         test.append(j['file_name'])
 #print(count(name))
-#for j in name:
-for j in test: 
-    #count=count+1
-    try:
 
+for j in test: 
+    try:
         src_dir = "/Users/sashrikasurya/Desktop/JPGs/"+str(j)
         print(src_dir)
         dst_dir = "/Users/sashrikasurya/Desktop/val"
         shutil.move(src_dir,dst_dir)
     except:
         print("Doesnt exist")
-    #with open(", "w") as outfile:
-print(count)
